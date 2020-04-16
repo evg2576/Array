965d0f4f36d8b9e6de0bd8d402554e29fe564b1e
Ivan Zamkovyi <izamkovyi@gmail.com>
2020-04-16 20:41:27 +0300

    feat: student skeleton

---
+++ Array.Tests/ArrayTasksTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ArrayObject;
+using NUnit.Framework;
+
+namespace Array.Tests
+{
+    [TestFixture]
+    public class ArrayTasksTest
+    {
+        [Test]
+        public void SimpleFalse()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [Test]
+        public void SimpleTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
